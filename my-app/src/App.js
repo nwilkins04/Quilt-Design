4678846c039507a60e807a70e9bbaaf3b6606e70
Nicole W <Nicole.Wilkins2018@gmail.com>
2019-01-18 00:51:11 -0500

    some style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,10 +7,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Square>
-          </Square>
+        <header className="App-header"> Design Your Quilt
         </header>
+          <div className="Body">
+            <div className="ColorDisplay">Colors?</div>
+            <div className="ShapeDisplay">Shapes?
+              <Square>
+
+              </Square>
+            </div>
+            <div className="QuiltDisplay">Hopefully quilt shows up here</div>
+          </div>
       </div>
     );
   }
