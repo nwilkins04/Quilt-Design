9e40b51fe1ab7496b089358e99f46dc9cf34bbf8
Nicole W <Nicole.Wilkins2018@gmail.com>
2019-01-17 19:48:02 -0500

    fixed error

---
+++ my-app/src/components/shapes/square.js
@@ -0,0 +1,16 @@
+import React from "react";
+import react-art;
+
+import "./square.css";
+
+// draw
+
+var Square = React.createClass({
+    render: function () {
+        return (
+            <View style={StyleSheet.square} />
+        )
+    }
+});
+
+export default Square;
