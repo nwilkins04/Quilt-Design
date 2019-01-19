27db42ac2ccebcf7cb93bc1ccb7cc88e7d3d384a
Nicole W <Nicole.Wilkins2018@gmail.com>
2019-01-19 12:42:35 -0500

    i think stuff added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './App.css';
 // import Square from "./components/shapes/shapes";
 import { Stage, Layer, Rect, Line } from "react-konva";
+import { SketchPicker } from "react-color";
+import Draggable, {DraggableCore} from "react-draggable";
 
 // const rectangle = () => {
 //   return(
@@ -27,36 +29,45 @@ class App extends Component {
       <div className="App">
         <header className="App-header"> Design Your Quilt
         </header>
-          <div className="Body">
-            <div className="ColorDisplay">Colors?</div>
-            <div className="ShapeDisplay">Shapes?
-            <Stage width={window.innerWidth} height={window.innerHeight}>
-              <Layer>
-                <Rect
-                x={20}
-                y={25}
-                width={200}
-                height={50}
-                stroke="black"
-                />
-                <Rect
-                x={20}
-                y={100}
-                width={50}
-                height={50}
-                stroke="black"
-                />
-                <Line
-                x={10}
-                y={125}
-                points={[55, 50, 10, 50, 55, 100]}
-                closed
-                stroke="black"
-                />
-              </Layer>
-            </Stage>
+          <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="ColorDisplay">Colors?
+                <SketchPicker />
+              </div>
+              <div className="ShapeDisplay">Shapes?
+                <Stage width={window.innerWidth} height={window.innerHeight}>
+                  <Layer>
+                    <Rect
+                    x={20}
+                    y={25}
+                    width={200}
+                    height={50}
+                    stroke="black"
+                    />
+                    <Rect
+                    x={20}
+                    y={100}
+                    width={50}
+                    height={50}
+                    stroke="black"
+                    />
+                    <Line
+                    x={10}
+                    y={125}
+                    points={[55, 50, 10, 50, 55, 100]}
+                    closed
+                    stroke="black"
+                    />
+                  </Layer>
+                </Stage>
+              </div>
             </div>
-            <div className="QuiltDisplay">Hopefully quilt shows up here</div>
+            <div className="col">
+              <div className="QuiltDisplay">Hopefully quilt shows up here
+              </div>
+            </div>
+            </div>
           </div>
       </div>
     );
