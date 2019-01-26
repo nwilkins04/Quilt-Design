419bb5a23d35b5f04986911f2be86e49ac21610e
Nicole W <Nicole.Wilkins2018@gmail.com>
2019-01-26 11:14:11 -0500

    have hexagon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,125 @@
 import React, { Component } from 'react';
 
+import { Stage, Layer, Rect, Line, } from "react-konva";
+
+import {CirclePicker } from 'react-color';
+
 import './App.css';
-import Shapes from "./components/shapes/shapes.js";
-import Colors from "./components/colors/colors.js";
-// import Draggable, {DraggableCore} from "react-draggable";
+// import Shapes from "./components/shapes/shapes.js";
+// import Colors from "./components/colors/colors.js";
 
 
 class App extends Component {
   state = {
-    isDragging:false
+    isDragging: false,
+    background: '#fff',
   };
+
+  handleChange(color, event) {
+    color ={
+      hex: '#333',
+      rgb: {
+        r: 51,
+        g: 51,
+        b: 51,
+        a: 1,
+      },
+      hsl: {
+        h: 0,
+        s: 0,
+        l: .20,
+        a: 1,
+      },
+    }
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header"> Design Your Quilt
-        </header>
+        <header className="App-header"> Design Your Quilt</header>
           <div className="container">
-          <div className="row">
-            <div className="col">
               <div className="ColorDisplay">Colors?
-                <Colors/>
+                {/* <Colors/> */}
+                <CirclePicker onChange={ this.handleChange}/>
               </div>
-              <div className="ShapeDisplay">Shapes?
-                <Shapes/>
-              </div>
-            </div>
-            <div className="col">
               <div className="QuiltDisplay">Hopefully quilt shows up here
               </div>
-            </div>
-            </div>
+              <div className="ShapeDisplay">Shapes?
+                {/* <Shapes/> */}
+                <Stage width={window.innerWidth} height={window.innerHeight}>
+                <Layer>
+                <Rect
+                    x={20}
+                    y={25}
+                    width={200}
+                    height={50}
+                    stroke="black"
+                    draggable
+                    onDragStart={() => {
+                        this.setState({
+                            isDragging: true
+                        });
+                    }}
+                    onDragEnd={() => {
+                        this.setState({
+                            isDragging: false
+                        });
+                    }} 
+                />
+                <Rect
+                    x={20}
+                    y={100}
+                    width={50}
+                    height={50}
+                    stroke="black"
+                    onDragStart={() => {
+                      this.setState({
+                          isDragging: true
+                      });
+                    }}
+                    onDragEnd={() => {
+                      this.setState({
+                          isDragging: false
+                      });
+                    }} 
+                />
+                <Line
+                    x={10}
+                    y={125}
+                    points={[55, 50, 10, 50, 55, 100]}
+                    closed
+                    stroke="black"
+                    onDragStart={() => {
+                      this.setState({
+                          isDragging: true
+                      });
+                    }}
+                    onDragEnd={() => {
+                      this.setState({
+                          isDragging: false
+                      });
+                    }} 
+                />
+                <Line
+                  x={30}
+                  y={250}
+                  points={[0, -5, 50, -5, 75, 40, 50, 85, 0, 85, -25, 40,]}
+                  stroke="black"
+                  closed
+                  onDragStart={() => {
+                    this.setState({
+                        isDragging: true
+                    });
+                  }}
+                  onDragEnd={() => {
+                    this.setState({
+                        isDragging: false
+                    });
+                  }} 
+                />
+            </Layer>
+                </Stage>
+              </div>
           </div>
       </div>
     );
