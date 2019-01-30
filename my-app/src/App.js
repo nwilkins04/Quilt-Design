2839be901733dab28d0722058e59d7292e259a29
Nicole W <Nicole.Wilkins2018@gmail.com>
2019-01-30 00:19:43 -0500

    added hexagons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,8 @@ import Rectangle from "./components/shapes/rectangle";
 import Square from "./components/shapes/square";
 import Triangle from "./components/shapes/triangle";
 import Hexagon from "./components/shapes/hexagon";
+import Hexagon2 from "./components/shapes/hexagon2";
+import Hexagon3 from "./components/shapes/hexagon3";
 import Rectangle2 from './components/shapes/reactangle2';
 
 class App extends Component {
@@ -20,7 +22,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header"> Design Your Quilt</header>
           <div className="container">
-              <div className="ShapeDisplay">Shapes
+              <div className="ShapeDisplay">
+                <h2>Shapes</h2>
                 <Stage width={window.innerWidth} height={window.innerHeight}>
                   <Layer>
                     <Rectangle/>
@@ -28,10 +31,14 @@ class App extends Component {
                     <Square/>
                     <Triangle/>
                     <Hexagon/>
+                    <Hexagon2/>
+                    <Hexagon3/>
                 </Layer>
                 </Stage>
               </div>
-              <div className="QuiltDisplay">Place Shapes Here to Make Your Quilt</div>
+              <div className="QuiltDisplay">
+                <h2>Place Shapes Here to Make Your Quilt</h2>
+              </div>
               <div className="ColorDisplay">
                   <Colors/>
               </div>
---
+++ my-app/src/components/shapes/hexagon3.js
@@ -0,0 +1,42 @@
+import React, {Component} from "react";
+import { Line } from "react-konva";
+import "./shapes.css";
+
+class Hexagon3 extends Component {
+    state = {
+        isDragging: false,
+        fill: ["lightgrey", "red", "orange", "yellow", "green", "blue", "purple", "brown", "black", "white"],
+        colorIndex: 0
+    };
+    
+    colorChange = () => {
+        let newIndex = (this.state.colorIndex + 1) % this.state.fill.length
+        this.setState({ colorIndex: newIndex})
+    }
+
+    render() {
+        return(
+        <Line onClick={(this.colorChange)}
+            x={250}
+            y={250}
+            points={[0, -5, 50, -5, 75, 40, 50, 85, 0, 85, -25, 40]}
+            closed
+            stroke="black"
+            draggable
+            fill={this.state.fill[this.state.colorIndex]}
+            onDragStart={() => {
+                this.setState({
+                    isDragging: true
+                });
+            }}
+            onDragEnd={() => (
+                this.setState({
+                    isDragging: false
+                })
+            )}
+        />
+        );
+    };
+}
+
+export default Hexagon3;
