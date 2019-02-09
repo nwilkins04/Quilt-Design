a7ff6fa6766dcda5b904bdadb093c05085b9bb9f
Nicole W <Nicole.Wilkins2018@gmail.com>
2019-02-09 11:31:41 -0500

    copy works color now fails

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Stage, Layer, } from "react-konva";
+import { Stage, Layer, Rect, } from "react-konva";
 
 import './App.css';
 
@@ -20,10 +20,55 @@ import Rectangle2 from './components/shapes/reactangle2';
 import Rectangle3 from "./components/shapes/rectangle3";
 import Diamond from "./components/shapes/diamond";
 
+const random = num => Math.floor(Math.random() * num) + 1;
+
+const newRectangle = () => ({
+  x: random(250),
+  y: random(300),
+  width: 50,
+  height: 100
+});
+
 class App extends Component {
   state = {
     isDragging: false,
+    canvas: [
+      {
+        x: 150,
+        y: 25,
+        width: 50,
+        height: 100
+      }
+    ],
+    fill: ["lightgrey", "red", "orange", "yellow", "green", "blue", "purple", "brown", "black", "white"],
+    draggable: true,
   };
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      canvas: [...prevState.canvas, { ...newRectangle() }]
+    }));
+  };
+
+  handleDragStart = e => {
+    e.target.setAttrs({
+      scaleX: 1.1,
+      scaleY: 1.1
+    });
+  };
+
+  handleDragEnd = e => {
+    e.target.to({
+      duration: 0.5,
+      scaleX: 1,
+      scaleY: 1,
+    });
+  };
+
+  // colorChange = () => {
+  //   let newIndex = (this.state.colorIndex + 1) % this.state.fill.length
+  //   this.setState({ colorIndex: newIndex})
+  // }
 
   render() {
     return (
@@ -37,6 +82,21 @@ class App extends Component {
                 <h2>Shapes</h2>
                 <Stage width={window.innerWidth} height={window.innerHeight}>
                   <Layer>
+                  {this.state.canvas.map(({ height, width, x, y }, key) => (
+                    <Rect
+                      key={key}
+                      x={x}
+                      y={y}
+                      width={width}
+                      height={height}
+                      stroke="black"
+                      draggable={this.state.draggable}
+                      fill="lightgrey"
+                      onDragStart={this.handleDragStart}
+                      onDragEnd={this.handleDragEnd}
+                      onClick={this.handleClick}
+                    />
+                  ))}
                     <Rectangle2/>
                     <Rectangle3/>
                     <Square/>
