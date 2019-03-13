6dfc244246fe2253260f781b2e4dde41d7b70e84
Nicole W <Nicole.Wilkins2018@gmail.com>
2019-03-13 13:39:11 -0400

    Updates

---
+++ precache-manifest.6fb0cb4de739803dde074026ba8ea022.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59368e126f33522d0b24",
+    "url": "/Quilt-Design/static/js/runtime~main.59368e12.js"
+  },
+  {
+    "revision": "1178b11ced7130df9390",
+    "url": "/Quilt-Design/static/js/main.1178b11c.chunk.js"
+  },
+  {
+    "revision": "882e5018993090a6ce11",
+    "url": "/Quilt-Design/static/js/1.882e5018.chunk.js"
+  },
+  {
+    "revision": "1178b11ced7130df9390",
+    "url": "/Quilt-Design/static/css/main.b254a3ad.chunk.css"
+  },
+  {
+    "revision": "b5763bbfa5c1c3fa8c283715b1d403d0",
+    "url": "/Quilt-Design/index.html"
+  }
+];
